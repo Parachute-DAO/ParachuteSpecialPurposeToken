77fc6324766a891348a8f7874f1ea139d2122b85
icemanparachute <acfmichelsen@gmail.com>
2022-04-16 16:33:16 -0400

    add pancake swap for testing

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,22 @@ const networks = require('./networks');
 const api = require('./apiKeys');
 
 module.exports = {
-  solidity: "0.8.13",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.13",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          }
+        },
+      },
+      {
+        version: "0.5.16",
+      }
+    ]
+  },
   networks: {
     bsc: {
       url: networks.bsc.url,
